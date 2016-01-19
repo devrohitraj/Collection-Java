400b089a5fc76d0dfd2b5fca6f97e8f463f157e0
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-19 16:38:32 +0530

    Create Box.java
    Project5

---
+++ Box.java
@@ -0,0 +1,41 @@
+package com.wipro.milestone1.TM4.projects.ques5;
+
+public class Box {
+	
+	private double length;
+	private double width;
+	private double height;
+	private double vol;
+	
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	
+	public double getVol() {
+		return vol;
+	}
+	public void setVol(double vol) {
+		this.vol = vol;
+	}
+	
+	
+	
+
+}
